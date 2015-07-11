f341c529bbb2e77500f544f5123ea8aa17e52e85
Brendan Duke <dukebw@mcmaster.ca>
2015-07-11 17:52:30 -0400

    derp

--- arm_disassembler.c
+++ arm_disassembler.c
@@ -34,7 +34,8 @@ int main(int argc, char **argv)
 
     // NOTE(brendan): Reset asserted and de-asserted
     ProgramCounter = 0;
-    // NOTE(brendan): Supervisor mode | ARM state | disable fast interrupts and normal interrupts
+	// NOTE(brendan): Supervisor mode | ARM state | disable fast interrupts and
+	// normal interrupts
     // disable imprecise aborts (v6 only).
     // TODO(brendan): CPSR[9] = CP15_reg1_EEbit endianness on exception entry
     Cpsr = 0x1D3;
@@ -42,64 +43,71 @@ int main(int argc, char **argv)
     // TODO(brendan): Thumb instructions
     uint32 InstructionLength = ARM_INSTRUCTION_LENGTH;
     uint32 NextInstruction;
-    uint32 *ProgramCounter = Registers + 15;
     // TODO(brendan): condition on when to stop executing?
-    for (*ProgramCounter = 2*ARM_INSTRUCTION_LENGTH;
+    for (ProgramCounter = 2*ARM_INSTRUCTION_LENGTH;
          ;
-         *ProgramCounter += InstructionLength)
+         ProgramCounter += InstructionLength)
     {
-        uint32 RomIndex = *ProgramCounter - 2*InstructionLength;
-        if (InstructionLength == ARM_INSTRUCTION_LENGTH)
-        {
-            NextInstruction = *(uint32 *)(Rom + RomIndex);
-            uint32 InstructionEncoding = (NextInstruction >> 20) & 0xFF;
+		do {
 
-            switch (InstructionEncoding)
-            {
-                // TODO(brendan): condition codes
-                case MOV_IMMEDIATE:
-                {
-                    uint32 RotateImmediate = 2*((NextInstruction >> 8) & 0xF);
-                    uint32 ByteImmediate = NextInstruction & 0xFF;
-                    uint32 ShifterOperand = (ByteImmediate >> RotateImmediate) |
-                                            (ByteImmediate << (32 - RotateImmediate));
-                    uint32 RegisterIndex = (NextInstruction >> 12) & 0xF;
-                    printf("mov r%d,#0x%x\n", RegisterIndex, ShifterOperand);
-                    Registers[RegisterIndex] = ShifterOperand;
-                    break;
-                }
-                case LOAD_IMMEDIATE:
-                {
-                    uint32 BaseRegister = (NextInstruction >> 16) & 0xF;
-                    uint32 RegisterIndex = (NextInstruction >> 12) & 0xF;
-                    uint32 Offset = NextInstruction & 0xFFF;
-                    printf("ldr r%d,[r%d + #0x%x]\n", RegisterIndex, BaseRegister,
-                                                      Offset);
-                    Registers[RegisterIndex] = *(uint32 *)(Rom + (Registers[BaseRegister] + Offset));
-                    break;
-                }
-                // TODO(brendan): add other branches, determined by bits [4:7]
-                case BRANCH_EXCHANGE:
-                {
-                    uint32 RegisterIndex = NextInstruction & 0xF;
-                    printf("bx r%d\n", RegisterIndex);
-                    *ProgramCounter = Registers[RegisterIndex] & 0xFFFFFFFE;
-                    if (Registers[RegisterIndex] & 1)
-                    {
-                        InstructionLength = THUMB_INSTRUCTION_LENGTH;
-                    }
-                    break;
-                }
-                default:
-                {
-                    Stopif(true, return 1, "Bad Instruction");
-                    break;
-                }
-            }
-        }
-        else
-        {
-        }
+
+			uint32 RomIndex = ProgramCounter - 2*InstructionLength;
+			if (InstructionLength == ARM_INSTRUCTION_LENGTH)
+			{
+				NextInstruction = *(uint32 *)(Rom + RomIndex);
+				uint32 InstructionEncoding = (NextInstruction >> 20) & 0xFF;
+
+				switch (InstructionEncoding)
+				{
+					// TODO(brendan): condition codes
+					case MOV_IMMEDIATE:
+					{
+						uint32 RotateImmediate = 2*((NextInstruction >> 8) & 0xF);
+						uint32 ByteImmediate = NextInstruction & 0xFF;
+						uint32 ShifterOperand = (ByteImmediate >> RotateImmediate) |
+							(ByteImmediate <<
+							 (32 - RotateImmediate));
+						uint32 RegisterIndex = (NextInstruction >> 12) & 0xF;
+						printf("mov r%d,#0x%x\n", RegisterIndex, ShifterOperand);
+						GeneralPurposeRegs[RegisterIndex] = ShifterOperand;
+						break;
+					}
+					case LOAD_IMMEDIATE:
+					{
+						uint32 BaseRegister = (NextInstruction >> 16) & 0xF;
+						uint32 RegisterIndex = (NextInstruction >> 12) & 0xF;
+						uint32 Offset = NextInstruction & 0xFFF;
+						printf("ldr r%d,[r%d + #0x%x]\n", RegisterIndex, BaseRegister,
+							   Offset);
+						GeneralPurposeRegs[RegisterIndex] =
+							*(uint32 *)(Rom + (GeneralPurposeRegs[BaseRegister] +
+											   Offset));
+						break;
+					}
+					// TODO(brendan): add other branches, determined by bits [4:7]
+					case BRANCH_EXCHANGE:
+					{
+						uint32 RegisterIndex = NextInstruction & 0xF;
+						printf("bx r%d\n", RegisterIndex);
+						ProgramCounter = GeneralPurposeRegs[RegisterIndex] &
+										 0xFFFFFFFE;
+						if (GeneralPurposeRegs[RegisterIndex] & 1)
+						{
+							InstructionLength = THUMB_INSTRUCTION_LENGTH;
+						}
+						break;
+					}
+					default:
+					{
+						Stopif(true, return 1, "Bad Instruction");
+						break;
+					}
+				}
+			}
+			else
+			{
+			}
+		} while (0);
     }
 
     fclose(InputBinary);
